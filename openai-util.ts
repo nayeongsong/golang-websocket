0e896dfbc7fd6c6fbf08f8da3dad9b1755533a1f
nayeongsong <nayeong.song@proton.me>
2024-11-23 16:44:45 +0200

    feat: update openai script

--- openai-util.ts
+++ openai-util.ts
@@ -2,6 +2,104 @@ import OpenAI from "openai"
 import fs from "fs"
 import * as dotenv from "dotenv"
 
+interface CitiesData {
+  [countryName: string]: CityData
+}
+
+interface CityData {
+  scores: Scores
+  details: Details
+  pros_cons: ProsCons
+  reviews: string[]
+  description: string | null
+  description_kor: string | null
+}
+
+interface Scores {
+  life_score: string
+  family_score: string
+  community_score: string
+  leisure_quality: string
+  education_index_score: string
+  english_speaking: string
+  walkScore_score: string
+  road_traffic_score: string
+  airlines_score: string
+  lost_luggage_score: string
+  happiness_score: string
+  nightlife: string
+  wifi_availability: string
+  places_to_work_score: string
+  ac_availability: string
+  friendliness_to_foreigners: string
+  female_friendly: string
+  lgbt_friendly: string
+  startup_score: string
+  apartment_cost: string
+  coworking_cost: string
+  average_meal_price: string
+  non_alcoholic_drink_in_cafe: string
+  beer_in_cafe: string
+  coffee_in_cafe: string
+  safety: string
+  food_safety: string
+  power_grid: string
+}
+
+interface Details {
+  total_score: DetailValue
+  cost: DetailValue
+  internet: DetailValue
+  temperature_now: DetailValue
+  humidity_now: DetailValue
+  income_level: DetailValue
+  people_density: DetailValue
+  continent: DetailValue
+  country: DetailValue
+  average_trip_length: DetailValue
+  internet_speed_avg: DetailValue
+  weather_now: DetailValue
+  travel_medical_insurance: DetailValueWithUrl
+  "1_000_xof_in_usd": DetailValue
+  suggested_atm_take_out: DetailValue
+  best_coworking_space: DetailValueWithUrl
+  best_coffee_place: DetailValueWithUrl
+  best_alt_coffee_place: DetailValueWithUrl
+  tap_water: DetailValueWithUrl
+  population: DetailValue
+  gdp_per_capita: DetailValue
+  population_density: DetailValue
+  gender_ratio_population: DetailValue
+  gender_ratio_young_adults: DetailValue
+  gender_ratio_nomads: DetailValue
+  religious_government: DetailValue
+  apartment_listings: DetailValueWithUrl
+  best_short_haul_air_carrier: DetailValueWithUrl
+  best_int_l_air_carrier: DetailValueWithUrl
+  best_hospital: DetailValueWithUrl
+  cost_of_living_for_nomad: DetailValue
+  cost_of_living_for_expat: DetailValue
+  cost_of_living_for_family: DetailValue
+  cost_of_living_for_local: DetailValue
+  airbnb_median_price: DetailValueWithUrl
+  hotel_median_price: DetailValue
+}
+
+interface DetailValue {
+  value: string
+}
+
+interface DetailValueWithUrl extends DetailValue {
+  url?: string
+  image_url?: string
+  logo_url?: string
+}
+
+interface ProsCons {
+  pros: string[]
+  cons: string[]
+}
+
 dotenv.config()
 
 const readCitiesJSONData = (jsonFilePath: string): CitiesData => {
@@ -28,21 +126,24 @@ const createCityDescription = async ({
   })
 
   const systemPrompt = `
-You are a helpful assistant specialized in generating brief descriptions of cities. Your responses should be in clear, user-friendly English, suitable for a general audience, including non-native speakers. Your task is to provide a balanced view of each city by prioritizing key aspects mentioned in user reviews. Supplement with additional city information only if the reviews lack certain details. Ensure the descriptions are concise, informative, and written in a friendly tone.
+You are a helpful assistant specialized in generating brief, engaging descriptions of cities. Your responses should be in clear, user-friendly English, suitable for a general audience, including non-native speakers.
 
-Focus on the following aspects, based on user reviews:
+Your task is to provide a balanced view of each city by prioritizing key aspects mentioned in user reviews and supplementing with additional information if needed. Ensure the descriptions are concise, informative, and written in a friendly, conversational tone.
+
+Focus on the following aspects:
 - Positive and negative aspects of living in the city.
 - Culture, cost of living, public transportation, and safety.
 - Notable attractions, unique characteristics, and local customs.
 - Challenges such as language barriers, high costs, or limited public transportation.
+- Practical tips for visitors and residents.
 
-If user reviews are too focused on "digital nomad" topics, generalize the information to make it relevant for all travelers and residents, not just digital nomads. If reviews do not cover these topics, add relevant city information to provide a complete overview:
+f user reviews are too focused on "digital nomad" topics, generalize the information to make it relevant for all travelers and residents. If reviews do not cover these topics, add relevant city information to provide a complete overview:
 
 - Natural scenery, amenities, and quality of life.
 - Popular activities and historical aspects.
 - Recommended neighborhoods, necessary precautions, or specific cultural considerations.
 
-Keep descriptions between 150 to 200 words and use simple language. Avoid making it sound too formal or AI-generated, and do not mention topics related to nomads.com or cannabis.
+Keep descriptions between 150 to 200 words and use simple language. Avoid making it sound too formal or AI-generated, and ensure it doesn't reference review sources directly. Additionally, DO NOT mention any topics related to "nomads.com" or cannabis.
 `
 
   const completion = await openai.chat.completions.create({
@@ -98,7 +199,7 @@ const extractDescriptionFromReviews = async (
 ) => {
   for (const city of Object.keys(citiesData)) {
     const reviews = citiesData[city].reviews
-    if (reviews.length !== 0 && !citiesData[city].description) {
+    if (reviews.length !== 0) {
       const description = await createCityDescription({
         cityName: city,
         countryName: citiesData[city].details.country.value,
@@ -127,10 +228,10 @@ const translateDescriptionToKorean = async (
 }
 
 const main = async () => {
-  const filePath = "./city_data_output/trimmed_output.json"
+  const filePath = "./city_data_output/output.json"
   const jsonData = readCitiesJSONData(filePath)
-  //await extractDescriptionFromReviews(jsonData, filePath)
-  await translateDescriptionToKorean(jsonData, filePath)
+  await extractDescriptionFromReviews(jsonData, filePath)
+  //await translateDescriptionToKorean(jsonData, filePath)
 }
 
 main()
